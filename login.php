7c3ecb7ea19709f80bdf1d18b1294234dd0e0429
Angezockt9980 <93945713+Angezockt9980@users.noreply.github.com>
2022-12-15 08:20:49 +0100

    Create login.php

---
+++ login.php
@@ -0,0 +1,21 @@
+<?php
+
+// Get the username and password from the form.
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+// Open the .htpasswd file.
+$file = fopen('.htpasswd', 'r');
+
+// Read each line of the file until the end is reached.
+while (!feof($file)) {
+  // Split the line into the username and password stored in the file.
+  $line = explode(':', fgets($file));
+
+  // If the username and password from the form match the ones in the file, the login is successful.
+  if ($username == $line[0] && $password == $line[1]) {
+    // TODO: Redirect the user to the logged in page.
+  }
+}
+
+// If the end of the file is reached without finding a match, the login is unsuccessful.
